2d6772eb635a3393b202d48596193980390028a2
duongwthuj <thunguyenduong37@gmail.com>
2026-04-05 10:35:22 +0700

    feat(dating): add useDatingDistance hook for location management
    - Add hook to request and update user location
    - Calculate distance between users
    - Format distance display (km/m)

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
 export { useFetch } from './useFetch';
 export { useDebounce } from './useDebounce';
 export { usePostActions } from './usePostActions';
+export { usePushNotifications } from './usePushNotifications';
+export { useDatingDistance } from './useDatingDistance';
